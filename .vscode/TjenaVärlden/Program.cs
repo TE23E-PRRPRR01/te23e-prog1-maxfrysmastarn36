2c388c56a780637cf981140d1e281492f9e52563
Tyr Lundström <tyr.lundstrom@elev.ga.ntig.se>
2024-08-26 14:31:55 +0200

    Jag har tränat lite saker mr sigma

---
+++ .vscode/TjenaVärlden/Program.cs
@@ -0,0 +1,6 @@
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+Console.WriteLine("Tjena grabben vad tycker du om att göra under fritiden? ");
+string bombaclaat = Console.ReadLine();
+Console.WriteLine(bombaclaat + "? Det är ju ganska laidback, tycker också om att göra sånt.");l
---
+++ Kapitel1/Regirisiteringing/Program.cs
@@ -0,0 +1,10 @@
+﻿// Program för riktiga sigmor
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för riktiga sigmor");
+Console.WriteLine("Förnamn?");
+string k = Console.ReadLine();
+Console.WriteLine("Efternamn?");
+string efter = Console.ReadLine();
+Console.WriteLine($"Tjena mr sigma också känd som {k} {efter}");
