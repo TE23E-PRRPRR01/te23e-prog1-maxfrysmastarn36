d3a52d7dd4bb77f6f729bd2a6885d13b9a231752
Tyr Lundström <tyr.lundstrom@elev.ga.ntig.se>
2024-09-02 14:51:53 +0200

    juste ja jag fixa lite if satser idag då

---
+++ Kapitel1/Lookcock/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Vad är ditt namn? ");
+string namn = Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Vad är din favorit ost? ");
+string ost = Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Favorit prilla? ");
+string prilla = Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Ange en kroppsdel? ");
+string kropp = Console.ReadLine();
+Console.Clear();
+
+if (namn == "" || ost == "" || prilla == "" || kropp == "")
+{
+    Console.WriteLine("fuck off");
+}
+else
+{
+    Console.WriteLine($"Hallå {namn}, jag tycker också {ost} är mycket smarrig.");
+    Console.WriteLine($"{prilla} är mycket lökig inte bra alls. {kropp} sånt händer allså");
+
+}
---
+++ Kapitel3/Engfchart/Program.cs
@@ -0,0 +1,79 @@
+﻿//sigma sigma on the wall who is the skibiest of them all
+Console.Clear();
+Console.WriteLine("Tja bror");
+
+Console.WriteLine($"""
+         ,     .
+        /(     )\               A
+   .--.( `.___.' ).--.         /_\
+   `._ `%_&%#%$_ ' _.'     /| <___> |\
+      `|(@\*%%/@)|'       / (  |L|  ) \
+       |  |%%#|  |       J d8bo|=|od8b L
+        \ \$#%/ /        | 8888|=|8888 |
+        |\|%%#|/|        J Y8P"|=|"Y8P F
+        | (.".)%|         \ (  |L|  ) /
+    ___.'  `-'  `.___      \|  |L|  |/
+  .'#*#`-       -'$#*`.       / )|
+ /#%^#%*_ *%^%_  #  %$%\    .J (__)
+ #&  . %%%#% ###%*.   *%\.-'&# (__)
+ %*  J %.%#_|_#$.\J* \ %'#%*^  (__)
+ *#% J %$%%#|#$#$ J\%   *   .--|(_)
+ |%  J\ `%%#|#%%' / `.   _.'   |L|
+ |#$%||` %%%$### '|   `-'      |L|
+ (#%%||` #$#$%%% '|            |L|
+ | ##||  $%%.%$%  |            |L|
+ |$%^||   $%#$%   |  VK/cf     |L|
+ |&^ ||  #%#$%#%  |            |L|
+ |#$*|| #$%$$#%%$ |\           |L|
+ ||||||  %%(@)$#  |\\          |L|
+ `|||||  #$$|%#%  | L|         |L|
+      |  #$%|$%%  | ||l        |L|
+      |  ##$H$%%  | |\\        |L|
+      |  #%%H%##  | |\\|       |L|
+      |  ##% $%#  | Y|||       |L|
+      J $$#* *%#% L  |E/
+      (__ $F J$ __)  F/
+      J#%$ | |%%#%L
+      |$$%#& & %%#|
+      J##$ J % %%$F
+       %$# * * %#&
+       %#$ | |%#$%
+       *#$%| | #$*
+      /$#' ) ( `%%\
+     /#$# /   \ %$%\
+    ooooO'     `Ooooo
+""");
+
+
+Console.WriteLine("Does it move? (y/n) ");
+string sigma = Console.ReadLine();
+
+if (sigma == "y")
+{
+    Console.WriteLine("Should it move? (y/n)");
+    string bomba = Console.ReadLine();
+    if (bomba == "y")
+    {
+        Console.WriteLine("Very nice");
+    }
+    else
+    if (bomba == "n")
+    {
+        Console.WriteLine("WD40");
+    }
+}
+else
+if (sigma == "n")
+{
+    Console.WriteLine("Should move? (y/n)");
+    string sigma2 = Console.ReadLine();
+    if (sigma2 == "y")
+    {
+        Console.WriteLine("Silver Tape");
+    }
+    else 
+    if (sigma2 == "n")
+    {
+        Console.WriteLine("Very nice");
+    }
+}
